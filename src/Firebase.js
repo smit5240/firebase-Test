96cc3a1eb375b38ebe3eb07993c72a602d32f9a6
smit5240 <121279568+smit5240@users.noreply.github.com>
2023-04-27 16:50:15 +0530

    fourth commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,8 @@
 import { getAuth } from "firebase/auth";
 import config from "./config";
 import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 firebase.initializeApp(config);
 export const db = firebase.firestore();
